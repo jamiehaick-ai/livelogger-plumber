374fc09bfc9b364f4e1556fba17d27c01b26fcdd
jamiehaick-ai <jamiehaick5@gmail.com>
2026-09-08 15:20:48 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bumping CACHE_NAME forces old cached files to be replaced on next load.
-const CACHE_NAME = "livelogger-plumber-v3";
+const CACHE_NAME = "livelogger-plumber-v4";
 const SHELL_FILES = [
   "./",
   "./index.html",
